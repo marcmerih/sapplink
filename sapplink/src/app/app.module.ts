941e8773996644a3d7371b534b7bec2459cab627
Marc Atasoy <merih.atasoy@gmail.com>
2020-04-15 19:38:48 -0400

    beginning router module

--- sapplink/src/app/app.module.ts
+++ sapplink/src/app/app.module.ts
@@ -5,18 +5,31 @@ import { UserComponent } from './applets/user-component/user.component';
 import { LoginComponent } from './applets/user-component/login/login.component';
 import { RegisterComponent } from './applets/user-component/register/register.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { IndexComponent } from './applets/index/index/index.component';
+import { RouterModule, Routes } from '@angular/router'
+
+const appRoutes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: 'login', component: UserComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
