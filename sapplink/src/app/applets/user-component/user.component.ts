9e199d5895a3de2b1256308ead391bbeb1a4adb1
Marc Atasoy <merih.atasoy@gmail.com>
2020-04-16 14:53:18 -0400

    visual login page changes complete

--- sapplink/src/app/applets/user-component/user.component.ts
+++ sapplink/src/app/applets/user-component/user.component.ts
@@ -12,16 +12,13 @@ export class UserComponent implements OnInit {
   userShouldLogin = true;
   loginForm: FormGroup;
   registerForm: FormGroup;
+
   constructor(private userService: UserService) { 
     this.loginForm = this.userService.getLoginForm();
     this.registerForm = this.userService.getRegisterForm();
   }
 
   ngOnInit(): void {
-  }
-
-  get loginOrRegister() {
-    return (this.userShouldLogin) ? 'Login' : 'Register';
   }
 
   // true represents register, false represents login
